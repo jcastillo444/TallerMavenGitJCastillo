d9bd70d64e12847a51e739824e97f44e3ca60e17
Jonathan Castillo <invef2015@gmail.com>
2021-02-18 17:50:13 -0500

    ejecuta el caso de prueba con el metodo indicando @Test

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,13 +1,22 @@
 package co.com.sofka.app.calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BasicCalculatorTest {
     private final BasicCalculator basicCalculator = new BasicCalculator();
+    @Test
     public void sum() {
         // Arrange
         Long number1 = 1L;
         Long number2 = 1L;
+        Long expectedValue = 2L;
 
         // Act
-        basicCalculator.sum(number1, number2);
+        Long result = basicCalculator.sum(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
     }
 }

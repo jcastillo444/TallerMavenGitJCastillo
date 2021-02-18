b71635e88a19760ebf68593c1e09e4d8e4d63410
Jonathan Castillo <invef2015@gmail.com>
2021-02-18 17:56:23 -0500

    Se realiza el test con multiples casos de prueba

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,12 +1,16 @@
 package co.com.sofka.app.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCalculatorTest {
     private final BasicCalculator basicCalculator = new BasicCalculator();
     @Test
+    @DisplayName("Testing sum: 1+1=2")
     public void sum() {
         // Arrange
         Long number1 = 1L;
@@ -18,5 +22,17 @@ public class BasicCalculatorTest {
 
         // Assert
         assertEquals(expectedValue, result);
+    }
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{1} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
     }
 }
